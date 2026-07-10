93bd7fcadd72219a9a6de0a35db67ae3a5e9a4b2
Janek <lxndr75@gmail.com>
2026-07-11 01:04:53 +0200

    fix: set basePath for github actions Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isGithubActions ? "/passionaut" : "",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
